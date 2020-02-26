87a1fe7a98742f45a99038f2e8ef6225d0e402d4
JHSaint <jordan.hewitta7@gmail.com>
2020-02-26 17:40:30 -0500

    Finished Lab 3!

--- app/views.py
+++ app/views.py
@@ -7,7 +7,14 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from flask_wtf.csrf import CSRFProtect 
+from app import mail
+from flask_mail import Message
+from .forms import ContactForm
 
+
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 ###
 # Routing for your application.
@@ -24,7 +31,23 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact/', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST':
+        if form.validate_on_submit() == True:
+            name = form.name.data
+            email = form.email.data
 
+            subject = form.subject.data
+            message = form.message.data
+
+            msg = Message(subject, sender=(name, email),recipients=["jordan.hewitta7@hotmail.com"])
+            msg.body = message
+            mail.send(msg)
+            flash('Success!')
+            return redirect(url_for('home'))
+    return render_template('contacts.html', form=form)
 ###
 # The functions below should be applicable to all Flask apps.
 ###
